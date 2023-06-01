0ab9694e1de0dc7ff691ccbeb8b501ffa26ac8e2
Dylan Williams <dylan.mi.williams@gmail.com>
2023-05-31 23:14:07 -0700

    updated db connection

--- server/db/db.js
+++ server/db/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: 'dylanwilliams',
-  password: '',
-  database: 'qanda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 pool.connect()
